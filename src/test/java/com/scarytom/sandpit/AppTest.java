8f264b83dd3f94313d9b286c122d61622c2fcab0
scarytom <t.denley@cantab.net>
2012-07-09 20:01:10 +0100

    initial commit

---
+++ src/test/java/com/scarytom/sandpit/AppTest.java
@@ -0,0 +1,14 @@
+package com.scarytom.sandpit;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public final class AppTest {
+
+    @Test public void
+    testApp() {
+        assertThat(1, is(1));
+    }
+}
